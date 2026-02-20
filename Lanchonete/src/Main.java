262105addde6d34e3533b34fed7b9e93ef4af73e
edson <edson@Edson>
2026-02-20 17:06:14 -0300

    Primeiro commit

---
+++ Lanchonete/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+     
+     Item item1 = new Item(" carne de boi", 12.99, 1);
+     Item item2 = new Item(" frango", 11.99, 2);
+     Item item3 = new Item("Coca-Cola", 5.99, 3);
+     Item item4 = new Item("Guarana", 4.99, 4);
+     
+     Carrinho c = new Carrinho();
+     
+     	c.adicionarItem(item2);
+     	c.adicionarItem(item1);
+     	c.adicionarItem(item3);
+     	c.adicionarItem(item4);
+     	
+     	c.removerItem(4);
+     	c.adicionarItem(item3);
+     	c.calcularPreco();
+     
+	}
+}
